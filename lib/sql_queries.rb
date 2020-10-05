cd7fbfe15a8296e24427e50c8d0c50a636f36cf8
doosemavis <moosedavis2011@gmail.com>
2020-10-05 15:57:05 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,9 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
+  "SELECT projects.title, SUM(pledges.amount) 
+  FROM projects JOIN pledges ON projects.id = pledges.project_id 
+  GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
@@ -17,7 +19,9 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT projects.title, projects.funding_goal SUM(pledges.amount)
+  FROM projects
+  GROUP BY projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
